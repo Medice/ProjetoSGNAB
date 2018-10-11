fd7832bd4d22138195a595cb69b02557d747e400
Lucas Cortez <lucascortez@ettbrasil.com.br>
2018-10-11 14:10:46 -0300

    Commit inicial

---
+++ projeto/sessao.php
@@ -0,0 +1,9 @@
+<?php
+ session_start();
+ if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+ {
+ unset($_SESSION['login']);
+ unset($_SESSION['senha']);
+ header('location:index.php');
+ }
+ ?>
