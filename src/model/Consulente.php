ffa93ca9b17bfc9001fd0bc4c44b0fda5d901018
aielloferraz <43560131+aielloferraz@users.noreply.github.com>
2018-10-27 13:06:32 -0300

    Add files via upload

---
+++ src/model/Consulente.php
@@ -0,0 +1,14 @@
+<?php
+class Consulente{
+    
+    public $id;
+    public $casoNab;
+    public $nomeConsulente;
+    public $tipoConsulente;
+    public $email;
+    public $endereco;
+    public $cep;
+    public $telefoneFixo;
+    public $telefoneCelular;
+
+}
